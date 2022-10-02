ee9eb7bb464f3a9d7ee14d9ed08ea0303489ac63
parmarsanjay9 <100059162+parmarsanjay9@users.noreply.github.com>
2022-10-02 16:13:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+
+noseX =0;
+noseY = 0;
+function preload(){
+    lip = loadImage('https://i.postimg.cc/PxFvYgkv/l1.png');
+}
+
+function setup() {
+    canvas = createCanvas(300, 250);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 250);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Intialized');
+}
+function gotPoses(results)
+{
+   if(results.length > 0)
+   {
+      console.log(results);
+      noseX = results[0].pose.nose.x-25;
+      noseY =  results[0].pose.nose.y+15;
+      console.log("lip x = " + noseX);
+      console.log("lip y = " + noseY);
+   }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 250);
+    image(lip , noseX, noseY, 50, 20);
+}
+
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
